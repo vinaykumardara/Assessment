fe35a3c52f94eb814c063b24dbf6369341908b66
vinaykumardara <vinay.darac3@gmail.com>
2023-01-25 18:15:18 +0530

    Client Changes

---
+++ CarrierPricingClient/CarrierPricingClient/GetQuotesRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace CarrierPricingClient
+{
+    public class GetQuotesRequest
+    {
+        public string Pickup_Postcode { get; set; }
+        public string Delivery_Postcode { get; set; }
+        public string Vehicle { get; set; }
+    }
+}
---
+++ CarrierPricingClient/CarrierPricingClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CarrierPricingClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var getQuotesRequest = new GetQuotesRequest
+            {
+                Pickup_Postcode = "SW1A1AA",
+                Delivery_Postcode = "EC2A3LT",
+                Vehicle = "bicycle"
+            };
+
+            var json = JsonSerializer.Serialize(getQuotesRequest);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var url = "https://localhost:44369/api/Pricing/quotes";
+            var client = new HttpClient();
+
+            var response = await client.PostAsync(url, data);
+
+            string result = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
